0eb5494a8db340459c207c0dd69744265162dfd8
192424375 <nitishachennapati@gmail.com>
2026-08-19 11:40:24 +0530

    Update 5EXPFODS.py

--- 5EXPFODS.py
+++ 5EXPFODS.py
@@ -1,3 +1,7 @@
+#5. Scenario: You are a data analyst working for a car manufacturing company. As part of your analysis,you have a dataset containing information about the fuel efficiency of different car models. The dataset
+#is stored in a NumPy array named fuel_efficiency, where each element represents the fuel efficiency(in miles per gallon) of a specific car model. Your task is to calculate the average fuel efficiency anddetermine the percentage improvement in fuel efficiency between two car models.
+#Question: How would you use NumPy arrays and arithmetic operations to calculate the average fuelefficiency and determine the percentage improvement in fuel efficiency between two car models?
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
