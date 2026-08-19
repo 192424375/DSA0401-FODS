1e93aa8a5b67bc20d07d55365320290fa65cb8fb
192424375 <nitishachennapati@gmail.com>
2026-08-19 11:55:14 +0530

    Add scenario and question comments for data analysis
    Added comments to clarify the scenario and question for data analysis.

--- exp17 .py
+++ exp17 .py
@@ -1,3 +1,6 @@
+#17. Scenario: You are a data analyst working for a company that sells products online. You have beentasked with analyzing the sales data for the past month. The data is stored in a Pandas data frame.
+#Question: Develop a code in python to find the frequency distribution of the ages of the customers whohave made a purchase in the past month.
+
 import pandas as pd
 import matplotlib.pyplot as plt
 
