507ba176918f76f054e3e4d9f75603b1d5e414f3
192424375 <nitishachennapati@gmail.com>
2026-08-19 11:35:42 +0530

    Update 2EXPFODS.py

--- 2EXPFODS.py
+++ 2EXPFODS.py
@@ -1,3 +1,6 @@
+#2. Scenario: You are a data analyst working for a company that sells products online. You have beentasked with analyzing the sales data for the past month. The data is stored in a NumPy array.
+#Question: How would you find the average price of all the products sold in the past month? Assume3x3 matrix with each row representing the sales for a different product
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
